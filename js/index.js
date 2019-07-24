7d12abba41dcf9d3d31cf8617005b281ac6f5092
Magamedrasul <curryrasul@gmail.com>
2019-07-24 15:50:28 +0300

    Added level

---
+++ js/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/* Канвас и контекст 2D */
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+/* Картинки */
+var gnd = new Image();
+var bg = new Image();
+
+gnd.src = "images/ground.png";
+bg.src = "images/back.svg";
+
+//Список существующих блоков
+var blocks = [];
+
+/* Отрисовка */
+function draw(){
+	ctx.clearRect(0 , 0 , canvas.width , canvas.height);
+	ctx.drawImage(bg , 0 , 0);
+	
+	parseMap();
+
+	for (var i = 0 ; i < blocks.length ; i++){
+		ctx.drawImage(gnd , blocks[i].x , blocks[i].y);
+	}
+}
+
+/* Парсер карты */
+function parseMap(){
+	//Если G ты это земля и определяем ее координаты
+	for (var i = 0; i < map.length ; i++){
+		for (var j = 0 ; j < map[i].length ; j++){
+			if (map[i][j] == ' '){
+				continue;
+			}
+			else if (map[i][j] == 'G'){
+				var o = {} ;
+				o.x = j * 32;
+				o.y = i * 32;
+			}
+			blocks.push(o);
+		}
+	}
+}
+
+var interval = setInterval(draw , 1);
---
+++ js/map.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var map = [
+	
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'GGG       GGGGGG           GGGGGGGGGGGGG',
+	'                                        ',
+	'                                        ',
+	'                                        ',
+	'GGGGGGG   GGGGGGGGGGGG    GGGGGGGGGGGGGG',
+	'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
+	'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
+	'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
+	'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
+	'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG'
+
+];
