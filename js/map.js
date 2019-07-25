13ab1434a2352a23f0b4a6f9653ce3a3110804aa
Igor Bakhtin <50614902+igor-vgs@users.noreply.github.com>
2019-07-26 01:58:31 +0300

    Add files via upload
    Adding first map generation

--- js/map.js
+++ js/map.js
@@ -1,7 +1,57 @@
 'use strict';
+//Генерация карты
+//Генерация происходит построчно с помощью Math.random()
 
-var map = [
-    
+//Это пока только набросок
+function matrixArray(rows,columns){
+  var arr = new Array();
+  for(var i=0; i<rows; i++){
+    arr[i] = new Array();
+    for(var j=0; j<columns; j++){
+      if( i < 3 ){                   //пустое пространство
+        arr[i][j] = ' ';
+      }else if( i > 2 && i < 7){    //генерация факелов
+        var k = Math.random();
+        if(k > 0.95){               //число можно изменять, поэксперементируете
+          arr[i][j] = 'F';
+        }else{
+          arr[i][j] = ' ';
+        }
+      }else if ( i > 6 && i < 12) { //пустое пространство
+        arr[i][j] = ' ';
+      }else if (i == 12) {          //блоки в воздухе
+        var k = Math.random();
+        if(k > 0.40){
+          arr[i][j] = 'G';
+        }else{
+          arr[i][j] = ' ';
+        }
+      }else if (i > 12 && i < 16) { //пустое пространство
+        arr[i][j] = ' ';
+      }else if (i == 16) {
+        var k = Math.random();
+        if(k > 0.80 && j > 2){     //Генерация ямы с копьями
+          arr[i][j] = ' ';
+        }else{
+          arr[i][j] = 'G';
+        }
+      }else if (i == 17) {         //Генерация самих копий
+        if(arr[i - 1][j] == ' '){
+          arr[i][j] = 'S';
+        }else {
+          arr[i][j] = 'G';
+        }
+      }else {
+        arr[i][j] = 'G';          //Генерация земли
+      }
+    }
+  }
+  return arr;
+}
+
+var map = matrixArray(22,40);
+
+/*var map = [
     '                                        ',
     '                                        ',
     '                   F                    ',
@@ -25,5 +75,4 @@ var map = [
     'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
     'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
     'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG'
-
-];
+];*/
