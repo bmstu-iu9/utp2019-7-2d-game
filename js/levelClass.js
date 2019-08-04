989bf7bd53869ad39bbe3764cd47266b20cd5d54
ValeriyaLebedeva <lvs_vs@mail.ru>
2019-08-04 22:00:00 +0300

    Fix cois

--- js/levelClass.js
+++ js/levelClass.js
@@ -2,4 +2,5 @@ var LevelClass = function(number) {
   this.number = number;
   this.allCoins = 0; //количество монеток всего, заполняться будет при парсинге карты
   this.currentCoins = 0; // будет увеличиваться, когда игрок собирет очередную монету
+  this.completed = false;
 }
