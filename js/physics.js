187c7b35f64a0e2d526892659a754ceb2ed20117
MiGame <ramil_1.9.9.9@mail.ru>
2019-08-10 18:32:21 +0600

    Fix glide bug

--- js/physics.js
+++ js/physics.js
@@ -4,7 +4,7 @@ function physics(){
         передвижение перса
     */
     if(rightPressed){ // исключение застриваний в потолке
-        var j = ~~((indi.x + 30) / 32); //Целочисленное деление )) Нашел в инете 
+        var j = ~~((indi.x + 20) / 32); //Целочисленное деление )) Нашел в инете 
         var i = ~~((indi.y + 75) / 32);
         if (((indi.y + 16) %  32 < 3) ||  ((indi.y + 16) %  32 > 29))  {
         	if(map[i - 1][j + 1] != 'G' && map[i][j + 1] != 'G'){
@@ -70,32 +70,59 @@ function physics(){
     if(leftPressed){
     	var j = ~~((indi.x + 30) / 32); //Целочисленное деление )) Нашел в инете
     	var i = ~~((indi.y + 75) / 32);
+    	if (((indi.y + 16) %  32 < 3) ||  ((indi.y + 16) %  32 > 29))  {
+	    	if(map[i - 1][j] != 'G' && map[i][j] != 'G'){
+	        	indi.x -= 1.5;
+	        	if(indi.stage == 0){
+	            	indi.hero.src = "images/maingonext.png";
+	            	indi.prevStage = 0;
+	            	indi.stage = 1;
+	        	} else if (indi.stage == 1 && indi.prevStage == 0){
+	          		indi.hero.src = "images/maingo.png";
+	          		indi.stage = 2;
+	          		indi.prevStage = 1;
+	        	} else if(indi.stage == 2){
+	            	indi.hero.src = "images/maingonext.png";
+	            	indi.stage = 1;
+	            	indi.prevStage = 2;
+	        	} else if(indi.stage == 1 && indi.prevStage == 2){
+	            	indi.hero.src = "images/mainpers.png";
+	            	indi.prevStage = 0;
+	            	indi.stage = 0;
+	        	}
 
-    	if(map[i - 1][j] != 'G' && map[i][j] != 'G'){
-        	indi.x -= 1.5;
-        	if(indi.stage == 0){
-            	indi.hero.src = "images/maingonext.png";
-            	indi.prevStage = 0;
-            	indi.stage = 1;
-        	} else if (indi.stage == 1 && indi.prevStage == 0){
-          		indi.hero.src = "images/maingo.png";
-          		indi.stage = 2;
-          		indi.prevStage = 1;
-        	} else if(indi.stage == 2){
-            	indi.hero.src = "images/maingonext.png";
-            	indi.stage = 1;
-            	indi.prevStage = 2;
-        	} else if(indi.stage == 1 && indi.prevStage == 2){
-            	indi.hero.src = "images/mainpers.png";
-            	indi.prevStage = 0;
-            	indi.stage = 0;
-        	}
+	    	}else {
+	    		indi.hero.src = "images/mainpers.png";
+	            indi.prevStage = 0;
+	            indi.stage = 0;
+	    	}
+	    } else {
+	    	if(map[i - 1][j] != 'G' && map[i][j] != 'G' && map[i - 2][j] != 'G'){
+	        	indi.x -= 1.5;
+	        	if(indi.stage == 0){
+	            	indi.hero.src = "images/maingonext.png";
+	            	indi.prevStage = 0;
+	            	indi.stage = 1;
+	        	} else if (indi.stage == 1 && indi.prevStage == 0){
+	          		indi.hero.src = "images/maingo.png";
+	          		indi.stage = 2;
+	          		indi.prevStage = 1;
+	        	} else if(indi.stage == 2){
+	            	indi.hero.src = "images/maingonext.png";
+	            	indi.stage = 1;
+	            	indi.prevStage = 2;
+	        	} else if(indi.stage == 1 && indi.prevStage == 2){
+	            	indi.hero.src = "images/mainpers.png";
+	            	indi.prevStage = 0;
+	            	indi.stage = 0;
+	        	}
 
-    	}else {
-    		indi.hero.src = "images/mainpers.png";
-            indi.prevStage = 0;
-            indi.stage = 0;
-    	}
+	    	}else {
+	    		indi.hero.src = "images/mainpers.png";
+	            indi.prevStage = 0;
+	            indi.stage = 0;
+	    	}
+	    }
 
         indi.direction = "left";
     }
