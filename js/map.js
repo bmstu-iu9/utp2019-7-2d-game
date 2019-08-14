35b884c362ab5339e4524230305ff0b9c20dcf60
Griffire <eaglegriff@mail.ru>
2019-08-14 17:22:22 +0300

    Проваливающиеся блоки

--- js/map.js
+++ js/map.js
@@ -12,21 +12,25 @@ var map = [
     'G                                      GGGGGGGGGGGGGGGGGGggg           GGG     G',
     'G F                                    GG                   g          G      GG',
     'G               S                      GG                    G         G       G',
-    'G      G        G                      GG                     G        G    GGGG',
+    'G               G                      GG                     G        G    GGGG',
     'G                    FFF  FFF  FFF     GG                              GG      G',
-    'G                @    FFF  FFF  FFF    GG                              GGG     G',
+    'G     G          @    FFF  FFF  FFF    GG                              GGG     G',
     'G           C    G     FFF  FFF  FFF   GG     GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
     'GGG       GGGGGG        FFF  FFF  FFF  GG                                      G',
     'G            G           FFF  FFF  FFF GG                                      G',
     'G                       FFF  FFF  FFF  GGG                                     G',
     'G                      FFF  FFF  FFF   GG                                      G',
-    'GGGGGGG   GGGGGGGGGGGGFFF GGGG  FFF    gg   GG                                 G',
-    'GGGGGGGSSSGGGGGGGGGGGGFF  GGGGGGFF     gg                                      G',
-    'GGGGGGGGGGGGGGGGGGGGGGSSSSGGGGGGGGGGGGGGG                                      G',
-    'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG      GGG                                    G',
-    'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG     G                                      G',
-    'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG'
-
+    'GGGGGGGgggGGGGGGGGGGGGFFF GGGG  FFF    gg   GG                                 G',
+    'GGGGGGGgggGGGGGGGGGGGGFF  GGGGGGFF     gg                                      G',
+    'GGGGGGGgggGGGGGGGGGGGGSSSSGGGGGGGGGGGGGGG                                      G',
+    'GGGGGGGgggGGGGGGGGGGGGGGGGGGGGGGGG      GGG                                    G',
+    'GGGGGGGgggGGGGGGGGGGGGGGGGGGGGGGGGG     G                                      G',
+    'GGGGGGGgggGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
+    '      G   G                                                                     ',
+    '      G   G                                                                     ',
+    '      G   G                                                                     ',
+    '      GSSSG                                                                     ',
+    '      GGGGG                                                                     '
 ];
 
 
