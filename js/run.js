96f7965c046ef08af9ccc86dac7d32c69c845fc9
Alexandr <alex.pisckunoff@yandex.ru>
2019-08-16 13:43:26 +0300

    Прислонившийся герой + Исправлена анимация бега влево

--- js/run.js
+++ js/run.js
@@ -38,7 +38,7 @@ const run = (DD) => {
           }
           indi.posision += 0.03; // смена картинок для бега
           indi.x ++;
-          if ((indi.x + DD[0]) >= 899 - indi.width) {
+          if ((indi.x + DD[0]) >= canvas.width * 3 / 4) {
             backGroundParse(1,0,DD); //сдвиг фона
           }
           indi.orientation = "r"; // смена ориентации
@@ -47,7 +47,7 @@ const run = (DD) => {
              indi.y += 2;
            }
            ctx.drawImage(indi.hero ,160 ,296 ,160 ,200 , indi.x , indi.y , 80 , 80); // анимация "столкновение со стеной"*/
-          ctx.drawImage(TestR , (indi.x + DD[0]), (indi.y + DD[1]),32 ,64 );
+          ctx.drawImage(TestR, (indi.x + DD[0]), (indi.y + DD[1]),32 ,64 );
         }
       }
       if (leftPressed) {
@@ -55,20 +55,20 @@ const run = (DD) => {
           indi.hero.src = "images/hero/HeroL.png";
           if (indi.airTime < 100) {  // проверка на состояние в воздухе(при изменение физики требует доработки)
             // ctx.drawImage(indi.hero ,800 - ((~~indi.posision) * 160) % 960 ,0 ,160 ,200 , indi.x , indi.y , 80 , 80); // анимация бега
-            ctx.drawImage(TestL , (indi.x + DD[0]) , (indi.y + DD[1]),32 ,64 );
+            ctx.drawImage(TestL, (indi.x + DD[0]), (indi.y + DD[1]), 32, 64);
           } else {
               // ctx.drawImage(indi.hero ,480 ,512 ,160 ,200 , indi.x , indi.y , 80 , 80); // анимация падения
-              ctx.drawImage(TestL , (indi.x + DD[0]) , (indi.y + DD[1]),32 ,64 );
+              ctx.drawImage(TestL , (indi.x + DD[0]), (indi.y + DD[1]), 32, 64);
           }
           indi.posision -= 0.03; // смена картинок для бега
           indi.x --;
-          if ((indi.x+ DD[0]) <= 300) {
+          if ((indi.x+ DD[0]) <= canvas.width / 4) {
             backGroundParse(-1,0,DD);
           }
           indi.orientation = "l"; // смена ориентации
         } else {
           // ctx.drawImage(indi.hero ,640 ,296 ,160 ,200 , indi.x , indi.y , 80 , 80); // анимация "столкновение со стеной"
-          ctx.drawImage(TestL , (indi.x + DD[0]) , (indi.y + DD[1]),32 ,64 );
+          ctx.drawImage(TestL, (indi.x + DD[0]), (indi.y + DD[1]), 32, 64);
         }
       }
     }
@@ -106,11 +106,11 @@ const run = (DD) => {
 
     indi.y -= indi.heroDY; //гравитационное смещение
 
-    if ((indi.y+ DD[1]) > 540) {
+    if ((indi.y + DD[1]) > canvas.height / 2) {
       backGroundParse(0, - indi.heroDY,DD); // коректировка фона по высоте
     }
 
-   if ((indi.y+ DD[1]) < 150) {
+   if ((indi.y + DD[1]) < canvas.height / 2) {
       backGroundParse(0, - indi.heroDY,DD); // коректировка фона по высоте
     }
 }
