d8687a8064d5bf00b63cd13d8bcb92f43fd4df32
Griffire <eaglegriff@mail.ru>
2019-08-31 19:18:31 +0300

    очистка кода

--- js/End.js
+++ js/End.js
@@ -1,8 +1,8 @@
 const End = () => {
+  /* выводим уведомление о конце игры */
   ctx.clearRect(0 , 0 , canvas.width , canvas.height);
   ctx.drawImage(bg , 0 , 0);
   ctx.strokeStyle = "white";
   ctx.font = 'bold 25px sans-serif';
   ctx.strokeText("Игра пройдена", 20, 45);
-  // End();
 }
